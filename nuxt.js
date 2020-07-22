a7e664d9079ee368f8e6f1c588dd627077be124b
jc <jc@makesense.org>
2020-07-22 09:06:35 +0200

    Nuxt import component

--- nuxt.js
+++ nuxt.js
@@ -3,7 +3,7 @@ import { join } from "path";
 export default function() {
   this.nuxt.hook("components:dirs", (dirs) => {
     dirs.push({
-      path: join(__dirname, "components"),
+      path: join(__dirname, "src/system/components"),
       prefix: "mks-ds",
     });
   });
