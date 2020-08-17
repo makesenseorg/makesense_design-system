a5a54523a3c6eb919da58864cd82aceef1c69827
Marine LB <marine.leborgne@live.fr>
2020-08-17 16:08:54 +0200

    DS-32: WIP create menu component

---
+++ src/system/components/molecules/SiteHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="site-header">
+    <div class="site-header__logo">{{ logo }}</div>
+    <div class="site-header__i18n">FR EN ES</div>
+    <nav class="site-header__menu">
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </nav>
+    <div class="site-header__sidebar-control">---</div>
+  </header>
+</template>
+<script>
+export default {
+  props: {
+    /* header background color */
+    theme: {
+      type: String
+    },
+    /* site logo */
+    logo: {
+      type: String
+    }
+  },
+  data() {}
+};
+</script>
+<style lang="scss" scoped></style>
