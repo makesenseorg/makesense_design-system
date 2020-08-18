f71af57e90eb2c5e4c2f5063e93f74ef8f783aa6
Marine LB <marine.leborgne@live.fr>
2020-08-18 13:10:13 +0200

    DS-32: WIP ajout de la stucture AppHeader du dossier athom

--- src/system/components/molecules/SiteHeader.vue
+++ src/system/components/molecules/SiteHeader.vue
@@ -3,8 +3,49 @@
     <div v-if="$slots['top-bar']" class="site-header__top-bar">
       <slot name="top-bar"></slot>
     </div>
-    <div class="site-header__logo">{{ logo }}</div>
-    <div class="site-header__i18n">FR EN ES</div>
+    <div class="site-header__bar">
+      <div class="site-header__user">
+        <mks-avatar></mks-avatar>
+        <AthText
+          :utils="[
+            'margin-left-default font-weight-bold font-size-small',
+            {
+              'color-default-reverse':
+                $route.meta.headerTranslucent && !$store.state.app.headerFixed
+            }
+          ]"
+          >{{ user }}</AthText
+        >
+        <div>
+          <AthText
+            tag="span"
+            :utils="[
+              'margin-left-default font-weight-light font-size-xsmall',
+              {
+                'color-default-reverse':
+                  $route.meta.headerTranslucent && !$store.state.app.headerFixed
+              }
+            ]"
+            >{{ __("Pas vous ?") }}</AthText
+          >
+          <mks-button
+            @click="$emit($logout)"
+            variation="link"
+            :utils="[
+              'font-weight-light font-size-xsmall',
+              {
+                'color-default-reverse':
+                  $route.meta.headerTranslucent && !$store.state.app.headerFixed
+              }
+            ]"
+            >{{ __("Déconnexion") }}</mks-button
+          >
+        </div>
+      </div>
+      <div class="site-header__logo">{{ logo }}</div>
+      <div class="site-header__i18n">FR EN ES</div>
+    </div>
+
     <nav class="site-header__menu">
       <ul>
         <li>1</li>
@@ -26,9 +67,29 @@ export default {
     /* site logo */
     logo: {
       type: String
+    },
+    user: {
+      type: String
     }
   },
-  data() {}
+  data() {},
+  methods: {
+    logout: function() {
+      this.$api.Auth.logout();
+      this.$router.push("home");
+    },
+    openMenu: function() {
+      this.$store.commit("nav/showSidebar");
+    }
+  }
 };
 </script>
 <style lang="scss" scoped></style>
+
+<docs>
+```jsx
+
+<site-header></site-header>
+
+```
+</docs>
