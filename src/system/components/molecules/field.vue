8f6ad3ffa66ed26c4c5d790f718b5cff47a216f1
Marine Le Borgne <marineleborgne@MacMarine.local>
2020-09-08 10:52:12 +0200

    NO-TICKET: update case of import

--- src/system/components/molecules/field.vue
+++ src/system/components/molecules/field.vue
@@ -444,7 +444,7 @@ export default {
     if (this.foldDefault) this.foldingOpen = true;
   },
   mounted() {
-    if (this.type === "editor") this.component = () => import(`./fieldEditor`);
+    if (this.type === "editor") this.component = () => import(`./FieldEditor`);
   }
 };
 </script>
