023569471ed007f3bc04c9174c219cec2c0a12db
Marine LB <marine.leborgne@live.fr>
2020-10-16 18:02:36 +0200

    DS-67: remove svg icons

--- src/library.js
+++ src/library.js
@@ -1,5 +1,6 @@
 import system from "./system";
 import { tokens, themes } from "./system/tokens";
+// todo remove utils
 import * as utils from "./system/utils";
 import * as mixins from "./system/mixins";
 
