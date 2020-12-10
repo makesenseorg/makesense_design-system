19b0c3f4be00d55b6045cd641fa600ac2d38e885
jc <jc@makesense.org>
2020-12-10 11:50:53 +0100

    DS-95 : Add select icon & cusor pointer

--- src/system/components/molecules/field/Field.vue
+++ src/system/components/molecules/field/Field.vue
@@ -25,21 +25,31 @@
         <slot name="action"></slot>
       </div>
     </div>
-    <select
+    <div
       v-if="type === 'select'"
-      v-model="theValue"
-      class="field__input field--select"
-      :disable="disable"
-      :ref="reference"
-      :class="getCss"
-      @blur="$emit('blur')"
-      @focus="$emit('focus')"
-      @change="$emit('change')"
+      class="field__input"
+      style="position: relative;"
     >
-      <option v-for="(item, index) in options" :key="index" :value="item.value">
-        {{ item.label }}
-      </option>
-    </select>
+      <select
+        v-model="theValue"
+        class="field--select"
+        :disable="disable"
+        :ref="reference"
+        :class="getCss"
+        @blur="$emit('blur')"
+        @focus="$emit('focus')"
+        @change="$emit('change')"
+      >
+        <option
+          v-for="(item, index) in options"
+          :key="index"
+          :value="item.value"
+        >
+          {{ item.label }}
+        </option>
+      </select>
+      <mks-icon type="chevron-down" class="input__select-icon" />
+    </div>
     <component
       v-else-if="type === 'editor'"
       :is="component"
@@ -619,6 +629,7 @@ textarea,
 }
 
 select {
+  @include cursor-pointer;
   position: relative;
   padding-right: 2em !important;
   background-image: url("data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23E3E3E3%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E");
@@ -627,7 +638,8 @@ select {
   background-size: 0.65em auto, 100%;
 }
 
-.input__search-icon {
+.input__search-icon,
+.input__select-icon {
   max-width: $space-m;
   position: absolute;
   right: $space-m;
@@ -649,7 +661,7 @@ select {
   ```jsx
     <mks-field name="text" type="text" label="Text" placeholder="Placeholder" value=""></mks-field>
     <mks-field name="number" type="number" label="Number" placeholder="Placeholder" value=""></mks-field>
-    <mks-field name="select" etype="select" label="Select" placeholder="Placeholder" v-bind:options="[{value: '1', label: 'Option 1'}, {value: '2', label: 'Option 2'}]" value=""></mks-field>
+    <mks-field name="select" type="select" label="Select" placeholder="Placeholder" v-bind:options="[{value: '1', label: 'Option 1'}, {value: '2', label: 'Option 2'}]" value=""></mks-field>
     <mks-field name="editor" type="editor" label="Editor" placeholder="Placeholder" value=""></mks-field>
     <mks-field name="textarea" type="textarea" label="Textarea" placeholder="Placeholder" value=""></mks-field>
     <mks-field name="checkbox" type="checkbox" label="Checkbox" checkbox-label="Checkbox label" value=""></mks-field>
