50622d322633a59b6fd5ccfb82da4560b81e6a43
jc <jc@makesense.org>
2021-02-19 10:08:36 +0100

    DS-73 : Unit Test for langPicker

---
+++ src/system/components/molecules/langPicker/spec.js
@@ -0,0 +1,52 @@
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import Comp from "./LangPicker.vue";
+
+import Button from "../../atoms/button/Button.vue";
+
+const localVue = createLocalVue();
+localVue.component("MksButton", Button);
+
+describe("langPicker.vue", () => {
+  it("matches snapshot", () => {
+    const wrapper = shallowMount(Comp, {
+      localVue,
+      propsData: {
+        langs: ["French", "English"],
+        active: "French",
+      },
+    });
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it("emits select lang event", async () => {
+    const wrapper = shallowMount(Comp, {
+      localVue,
+      propsData: {
+        langs: ["French", "English"],
+        active: "French",
+      },
+    });
+
+    const button = wrapper.find(".app-lang__button--active");
+    expect(button.exists()).toBe(true);
+
+    button.trigger("click");
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.emitted("select")).toBeTruthy();
+  });
+
+  it("displays active lang modifier", () => {
+    const wrapper = shallowMount(Comp, {
+      localVue,
+      propsData: {
+        langs: ["French", "English"],
+        active: "French",
+      },
+    });
+
+    const button = wrapper.find(".app-lang__button--active");
+    expect(button.exists()).toBe(true);
+    expect(button.text()).toBe("French");
+  });
+});
