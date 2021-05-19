bc7853b303271d64891151fd49c8ffb0f5c75f8b
Marine LB <marine.leborgne@live.fr>
2021-05-19 12:33:38 +0200

    DS-149: test App in playground

--- src/playground/layouts/default.vue
+++ src/playground/layouts/default.vue
@@ -1,9 +1,23 @@
 <template>
-  <div></div>
+  <mks-app>
+    <mks-site-header 
+      slot="top" 
+      :menu-links="[{label: 'home', to: '/'}, {label: 'event', to: '/event'}]"
+      @openSidebar="toggleSidebar"
+    ></mks-site-header>
+    <Nuxt></Nuxt>
+    <mks-sidebar :open="sidebarOpen" @close="toggleSidebar">test</mks-sidebar>
+    <mks-site-footer slot="bottom">Made by makesense</mks-site-footer>
+  </mks-app>
 </template>
 <script>
+import MksApp from '@makesenseorg/design-system/dist/components/App'
+import MksSiteHeader from '@makesenseorg/design-system/dist/components/SiteHeader'
+import MksSidebar from '@makesenseorg/design-system/dist/components/Sidebar'
+import MksSiteFooter from '@makesenseorg/design-system/dist/components/SiteFooter'
 
 export default {
+  components: {MksApp, MksSiteHeader, MksSidebar, MksSiteFooter},
   data() {
     return {
       sidebarOpen: false
--- src/playground/pages/index.vue
+++ src/playground/pages/index.vue
@@ -7,12 +7,19 @@
       <mks-text>Hover on the loading to see a localized title</mks-text>
       <mks-loading type="simple"></mks-loading>
     </div>
+    <div class="mks-grid-auto-lg mks-grid-gap-sm">
+      <mks-card v-for="i in 12" :key="i" image="https://via.placeholder.com/500" image-format="square">
+        <template v-slot:title>Content</template>
+      </mks-card>
+    </div>
     
   </div>
 </template>
 
 <script>
+import MksCard from '@makesenseorg/design-system/dist/components/Card'
 export default {
+  components: {MksCard},
   created() {
     this.$MKSaddLocale('it', {
       loading: 'Caricamento...',
