9bbaa09d10d1ea6b541fb02fe690c5fdef2eec71
Marine LB <marine.leborgne@live.fr>
2021-08-10 12:35:17 +0200

    NO-TICKET: add spacing and no marker option to MksList

--- src/system/components/molecules/list/List.vue
+++ src/system/components/molecules/list/List.vue
@@ -1,7 +1,8 @@
 <template>
   <component
     :is="type === 'ordered' ? 'ol' : 'ul'"
-    :class="`list list--theme-${theme}`"
+    :class="classes"
+    :style="style"
   >
     <slot></slot>
   </component>
@@ -11,22 +12,58 @@
 export default {
   name: "MksList",
   props: {
+    /**
+     * To change the marker : bean (default) or none, to change the tag : ordered (unordered by default)
+     */
     type: {
       type: String,
       default: "bean",
       validator: value => {
-        return value.match(/(bean|ordered|bullet)/);
+        return value.match(/(bean|ordered|none)/);
       }
     },
+    /**
+     * Theme of the bean (primary, secondary, tertiary, neutral)
+     */
     theme: {
       type: String,
       default: "primary"
+    },
+    /**
+     * Spacing between elements (xxxs, xxs, xs (default), s, m, l, xl, xxl, xxxl)
+     */
+    spacing: {
+      type: String,
+      default: "xs"
+    }
+  },
+  computed: {
+    style() {
+      return `--spacing: var(--space-${this.spacing})`;
+    },
+    classes() {
+      let classes = "list";
+      if (this.type === "bean") {
+        classes += ` list--bean-${this.theme}`;
+      }
+      if (this.type === "none") {
+        classes += ` list--none`;
+      }
+      return classes;
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.list {
+  & > * + * {
+    margin-top: var(--spacing);
+  }
+}
+.list--none {
+  list-style-type: none;
+}
 $themes: (
   "neutral": $color-neutral-70,
   "primary": $color-primary,
@@ -35,7 +72,7 @@ $themes: (
 );
 
 @each $name, $variable in $themes {
-  .list--theme-#{$name} {
+  .list--bean-#{$name} {
     & > * {
       list-style-type: none;
       align-items: center;
@@ -48,6 +85,9 @@ $themes: (
 </style>
 <docs>
 
+### Default list
+Has an `ul` tag and bean as marker
+
 ```jsx
 <mks-list>
     <li>One</li>
@@ -55,5 +95,27 @@ $themes: (
     <li>Three</li>
     <li>Four</li>
 </mks-list>
-````
+```
+
+### Without marker
+
+```jsx
+<mks-list type="none">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+    <li>Four</li>
+</mks-list>
+```
+
+### With custom spacing
+
+```jsx
+<mks-list spacing="xl">
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+    <li>Four</li>
+</mks-list>
+```
 </docs>
